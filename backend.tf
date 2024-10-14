c4729a8933f4d737f5a5ac9ccf9abb968f6d64c6
nirav843 <nirav.p.desai@slscorp.com>
2024-10-14 15:08:02 +0530

    added source

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "three-tier-architecture-bucket"
+    bucket = "three-tier-architecture-bucket1611"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
